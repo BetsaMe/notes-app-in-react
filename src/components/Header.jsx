9da101c19c0c79c376059354028b263b017b9140
Betsa Meneses <betsadmd@gmail.com>
2022-11-13 16:10:07 +0100

    first version notes app

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+
+    return (
+        <div className="header">
+            <h1>Notes app 📔📝</h1>
+            <p>React course Scrimba</p>
+        </div>
+    )
+}
